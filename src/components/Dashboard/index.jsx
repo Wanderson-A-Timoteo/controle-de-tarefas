88e77f4889c79e4ed6631733b77627f01fd7d543
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-28 13:59:38 -0400

    Adicionado estilo ao componente Dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { ApiClient } from "adminjs";
+import { ApiClient, useTranslation } from "adminjs";
 import { Box, H2, Text } from "@adminjs/design-system";
 
 
 const api = new ApiClient();
 
 const Dashboard = () => {
+    const { translateMessage } = useTranslation();
     const [data, setData] = useState({});
 
     useEffect(() => {
@@ -15,7 +16,34 @@ const Dashboard = () => {
     }, []);
     return(
         <Box>
-            <Text>Dashboard</Text>
+            <Box position="relative" overflow="hidden">
+                <Box bg="grey20" height={284} py={74} px={["default", "lg", 250]}>
+                    <Text textAlign="center" color="primary100">
+                        <H2>{translateMessage("dashboardTitle")}</H2>
+                        <Text opacity="0.8">{translateMessage("dashboardSubTitle")}</Text>
+                    </Text>
+                </Box>
+            </Box>
+
+            <Box 
+                mt={["xl", "xl", "-80px"]}
+                mb="xl"
+                mx={[0, 0, 0, "auto"]}
+                px={["default", "lg", "0"]}
+                position="relative"
+                flex
+                flexDirection="row"
+                flexWrap="wrap"
+                width={[1, 1, 1, 1024]}
+            >
+                <Box width={[1, 1 / 2, 1 / 2]} p="lg">
+                    <Text>Chart 1</Text>
+                </Box>
+
+                <Box width={[1, 1 / 2, 1 / 2]} p="lg">
+                    <Text>Chart 2</Text>
+                </Box>
+            </Box>
         </Box>
     );
 };
